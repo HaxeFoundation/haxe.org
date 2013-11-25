bf3abc080d4c04b6b522986f8066e9382d01cec0
Jason O'Neil <jason.oneil@gmail.com>
2013-11-26 00:05:42 +0800

    Manual update

---
+++ assets/uf-content/website-content/manual/assets/MyAbstract.hx
@@ -0,0 +1,12 @@
+abstract AbstractInt(Int) {
+	inline public function new(i:Int) {
+		this = i;
+	}
+}
+
+class MyAbstract {
+	static public function main() {
+		var a = new AbstractInt(12);
+		trace(a); //12
+	}
+}
