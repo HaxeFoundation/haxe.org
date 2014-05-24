75e21b874af9bdb00e392d80d84aca23cf74d717
Jason O'Neil <jason.oneil@gmail.com>
2014-05-24 13:41:02 +0800

    Leave Tasks empty for now

--- src/Tasks.hx
+++ src/Tasks.hx
@@ -1,23 +1,12 @@
-import ufront.tasks.TaskSet;
 import haxe.web.Dispatch;
 import ufront.auth.model.*;
+import ufront.tasks.UFTaskSet;
 import sys.FileSystem;
 import tasks.*;
 
-class Tasks extends TaskSet
+class Tasks extends UFTaskSet
 {
-	@help("Pull a new version of the manual, parse it and save the output.") 
-	function doPullManual( ?deleteFirst=false ) {
-		
-	}
-	
-	@help("Pull a new version of the site, update the content.") 
-	function doPullSite() {
-		
-	}
-
 	static function main() {
-		sys.db.Manager.cnx = sys.db.Mysql.connect( app.Config.db );
-		TaskSet.run( new Tasks(), Sys.args() );
+		trace( "Nothing here yet" );
 	}
 }
