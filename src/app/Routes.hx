10b2d17ff4f0be03a531fc360b448f69f4cbbf2a
Jason O'Neil <jason.oneil@gmail.com>
2015-11-26 14:00:42 +1100

    Changes for client side compilation

--- src/app/Routes.hx
+++ src/app/Routes.hx
@@ -14,6 +14,7 @@ class Routes extends Controller
 	// Perform init() after dependency injection has occured.
 	@post public function init() {
 		// All MVC actions come through Routes (our index controller) first, so this is a good place to set global template variables.
+		#if server
 		var repo = context.request.scriptDirectory+Config.app.siteContent.folder;
 		var sitemap = pageApi.getSitemap( repo );
 		var r = context.request;
@@ -24,6 +25,7 @@ class Routes extends Controller
 		ViewResult.globalValues.set( "navBar", sitemap.printSitemap(NavBar,"/",r.uri) );
 		ViewResult.globalValues.set( "siteMap", sitemap.printSitemap(Footer,"/",r.uri) );
 		ViewResult.globalValues.set( "pageUrl", url );
+		#end
 		ViewResult.globalValues.set( "todaysDate", Date.now() );
 		ViewResult.globalValues.set( "currentYear", Date.now().getFullYear() );
 		ViewResult.globalValues.set( "description", "Haxe is an open source toolkit based on a modern, high level, strictly typed programming language." );
