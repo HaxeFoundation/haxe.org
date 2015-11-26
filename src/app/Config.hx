2c11547ddf64725dc5de22715eb2c82fd0c37382
Jason O'Neil <jason.oneil@gmail.com>
2015-11-26 14:09:08 +1100

    Add `staging` deployment target, and separate mysql config for server

--- src/app/Config.hx
+++ src/app/Config.hx
@@ -5,6 +5,10 @@ class Config
 	public static var app = CompileTime.parseJsonFile( "conf/app.json" );
 
 	#if server
-		public static var db = CompileTime.parseJsonFile( "conf/mysql.json" );
+		#if deploy
+			public static var db = CompileTime.parseJsonFile( "conf/mysql-haxeorg.json" );
+		#else
+			public static var db = CompileTime.parseJsonFile( "conf/mysql.json" );
+		#end
 	#end
 }
