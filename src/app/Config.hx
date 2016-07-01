3298bae34af39fe4eb23cd124b0dfe8b686a0423
Andy Li <andy@onthewings.net>
2016-07-01 13:16:04 +0800

    let db schema name be configurable too (HAXEORG_DB_NAME)

--- src/app/Config.hx
+++ src/app/Config.hx
@@ -6,12 +6,12 @@ class Config
 
 	#if server
 		public static var db = {
-			"host": Sys.getEnv("HAXEORG_DB_HOST"),
-			"port": Std.parseInt(Sys.getEnv("HAXEORG_DB_PORT")),
-			"database": "haxe",
-			"user": Sys.getEnv("HAXEORG_DB_USER"),
-			"pass": Sys.getEnv("HAXEORG_DB_PASS"),
-			"socket": null
+			"host":     Sys.getEnv("HAXEORG_DB_HOST"),
+			"port":     Std.parseInt(Sys.getEnv("HAXEORG_DB_PORT")),
+			"database": Sys.getEnv("HAXEORG_DB_NAME"),
+			"user":     Sys.getEnv("HAXEORG_DB_USER"),
+			"pass":     Sys.getEnv("HAXEORG_DB_PASS"),
+			"socket":   null
 		}
 	#end
 }
