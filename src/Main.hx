14f4241ea8ff0d07e6f8b3836d4d9abd8347f500
Valentin Lemière <valentin@ibilon.com>
2016-12-12 10:53:49 +0100

    Make sure the out folder exists

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,5 @@
+import sys.FileSystem;
+
 @:build(ViewBuilder.build())
 class Main {
 
@@ -5,6 +7,11 @@ class Main {
 		Sys.println("== haxe.org generation ==");
 		Sys.println('Output folder: "${Config.outputFolder}"');
 		var start = Date.now().getTime();
+
+		// Make sure the output folder exists
+		if (!FileSystem.exists(Config.outputFolder)) {
+			FileSystem.createDirectory(Config.outputFolder);
+		}
 
 		// Generating the content
 		SiteMap.init();
