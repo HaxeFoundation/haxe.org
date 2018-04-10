6e88c379a55ccc1a0fdff2405a3ed9d5d21682ab
Valentin Lemière <valentin@ibilon.com>
2018-04-10 17:33:14 +0200

    cpp, as3, js and sh grammars

--- src/SyntaxHighlighter.hx
+++ src/SyntaxHighlighter.hx
@@ -1,27 +1,45 @@
-import haxe.io.Path;
-import haxe.xml.Parser.XmlParserException;
+import haxe.Json;
 import highlighter.Highlighter;
-import sys.FileSystem;
 import sys.io.File;
-
-using StringTools;
 
 class SyntaxHighlighter
 {
-	static var haxeGrammar = new Highlighter("grammars/haxe-TmLanguage/haxe.tmLanguage");
-	static var hxmlGrammar = new Highlighter("grammars/haxe-TmLanguage/hxml.tmLanguage");
-	static var luaGrammar = new Highlighter("grammars/lua.tmbundle/Syntaxes/Lua.plist");
-	static var xmlGrammar = new Highlighter("grammars/xml.tmbundle/Syntaxes/XML.plist");
-	static var grammars : Map<String, Highlighter>;
-	static var missingGrammars = ["" => true];
-
 	public static function patch () {
 		Sys.println("Applying syntax highlighting ...");
 
-		grammars = ["haxe" => haxeGrammar, "hxml" => hxmlGrammar, "lua" => luaGrammar, "xml" => xmlGrammar];
+		// Convert CSON grammar to json for vscode-textmate
+		File.saveContent("bin/javascript.json", Json.stringify(CSON.parse(File.getContent("grammars/language-javascript/grammars/javascript.cson"))));
+		File.saveContent("bin/shell-unix-bash.json", Json.stringify(CSON.parse(File.getContent("grammars/language-shellscript/grammars/shell-unix-bash.cson"))));
+
+		var haxeGrammar = new Highlighter("grammars/haxe-TmLanguage/haxe.tmLanguage");
+		var hxmlGrammar = new Highlighter("grammars/haxe-TmLanguage/hxml.tmLanguage");
+		var luaGrammar = new Highlighter("grammars/lua.tmbundle/Syntaxes/Lua.plist");
+		var xmlGrammar = new Highlighter("grammars/xml.tmbundle/Syntaxes/XML.plist");
+		var cppGrammar = new Highlighter("grammars/c.tmbundle/Syntaxes/C++.plist");
+		var as3Grammar = new Highlighter("grammars/actionscript3-tmbundle/Syntaxes/ActionScript 3.tmLanguage");
+		var jsGrammar = new Highlighter("bin/javascript.json");
+		var shGrammar = new Highlighter("bin/shell-unix-bash.json");
+
+		var grammars = [
+			"haxe" => haxeGrammar,
+			"hxml" => hxmlGrammar,
+			"lua" => luaGrammar,
+			"xml" => xmlGrammar,
+			"cpp" => cppGrammar,
+			"as3" => as3Grammar,
+			"js" => jsGrammar,
+			"javascript" => jsGrammar,
+			"sh" => shGrammar,
+		];
 
 		// Go over the generated HTML file and apply syntax highlighting
-		patchFolder(Config.outputFolder);
+		var missingGrammars = Highlighter.patchFolder(Config.outputFolder, grammars, function (classList) {
+			return classList.substr(12);
+		});
+
+		for (g in missingGrammars) {
+			Sys.println('Missing grammar for "${g}"');
+		}
 
 		// Add CSS rules for highlighting
 		var path = Config.outputFolder + "/css/style.css";
@@ -29,84 +47,9 @@ class SyntaxHighlighter
 		var syntaxStyle = haxeGrammar.runCss();
 		File.saveContent(path, baseStyle + syntaxStyle);
 	}
+}
 
-	static function patchFolder (path:String) {
-		for (entry in FileSystem.readDirectory(path)) {
-			var entry_path = Path.join([path, entry]);
-
-			if (FileSystem.isDirectory(entry_path)) {
-				patchFolder(entry_path);
-			} else if (Path.extension(entry_path) == "html") {
-				patchFile(entry_path);
-			}
-		}
-	}
-
-	static function patchFile (path:String) {
-		try
-		{
-			var xml = Xml.parse(File.getContent(path));
-			processNode(xml);
-
-			var result = ~/&amp;([a-z]+;)/g.replace(xml.toString(), "&$1");
-			File.saveContent(path, result);
-		} catch (e:Dynamic) {
-			if (Std.is(e, XmlParserException)) {
-				var e = cast(e, XmlParserException);
-				Sys.println('${e.message} at line ${e.lineNumber} char ${e.positionAtLine}');
-				Sys.println(e.xml.substr(e.position - 20, 40));
-			} else {
-				Sys.println(e);
-			}
-
-			Sys.println(haxe.CallStack.toString(haxe.CallStack.exceptionStack()));
-			throw('Error when parsing "$path"');
-		}
-	}
-
-	static function processNode (xml:Xml) {
-		if (xml.nodeType == Xml.Element) {
-			switch (xml.nodeName) {
-				case "pre":
-					var code = xml.firstChild();
-
-					if (code.nodeType != Xml.Element)
-					{
-						return;
-					}
-
-					var lang = code.exists("class") ? code.get("class").substr(12) : ""; // in the form class="prettyprint lang"
-
-					if (grammars.exists(lang))
-					{
-						var original = code.firstChild().toString().htmlUnescape();
-						var highlighted = grammars.get(lang).runContent(original);
-						var new_xml = Xml.parse(highlighted);
-						var siblings = [for (n in xml.parent) n];
-						xml.parent.insertChild(new_xml, siblings.indexOf(xml));
-						xml.parent.removeChild(xml);
-					}
-					else if (!missingGrammars.exists(lang))
-					{
-						Sys.println('Missing grammar for "${lang}"');
-						missingGrammars.set(lang, true);
-					}
-
-				default:
-					processChildren(xml);
-			}
-		}
-
-		if (xml.nodeType == Xml.Document) {
-			processChildren(xml);
-		}
-	}
-
-	static function processChildren (xml:Xml) {
-		var children = [for (n in xml) n];
-
-		for (element in children) {
-			processNode(element);
-		}
-	}
+@:jsRequire("cson-parser")
+extern class CSON {
+	static function parse (content:String) : Dynamic;
 }
