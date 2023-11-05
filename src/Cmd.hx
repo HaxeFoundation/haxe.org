9c8101e9b2018babd64665dbfa770aa94cd0528b
Valentin Lemière <valentin@ibilon.com>
2023-11-05 11:39:25 +0100

    Fix interp cmd, remove officially deprecated atom ide

--- src/Cmd.hx
+++ src/Cmd.hx
@@ -1,13 +1,22 @@
-import js.node.ChildProcess;
-
 class Cmd {
 	static public function cmd(cmd:String, ?params:Array<String>):Void {
 		Sys.println('run: $cmd ${params.join(" ")}');
-		var result = ChildProcess.spawnSync(cmd, params == null ? [] : params, { shell: true });
+
+		#if nodejs
+		var result = js.node.ChildProcess.spawnSync(cmd, params == null ? [] : params, { shell: true });
 		if (result.status != 0) {
 			Sys.println(result.stdout);
 			Sys.println(result.stderr);
 			throw 'Error running $cmd $params';
 		}
+		#else
+		var process = new sys.io.Process(cmd, params);
+		if (process.exitCode() != 0) {
+			Sys.println(process.stdout.readAll().toString());
+			Sys.println(process.stderr.readAll().toString());
+			throw 'Error running $cmd $params';
+		}
+		process.close();
+		#end
 	}
 }
